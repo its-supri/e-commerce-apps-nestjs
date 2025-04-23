335320fb7f6d7e13bbd07b4fed6bb8321b85e734
l200160149 <ums.informatika@gmail.com>
2025-04-23 12:33:03 +0700

    implement configuration (.env)

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -1,20 +1,19 @@
-
 import { DataSource } from 'typeorm';
+import { ConfigService } from '@nestjs/config';
 
 export const databaseProviders = [
   {
     provide: 'DATA_SOURCE',
-    useFactory: async () => {
+    inject: [ConfigService],
+    useFactory: async (configService: ConfigService) => {
       const dataSource = new DataSource({
-        type: 'postgres',
-        host: 'localhost',
-        port: 5432,
-        username: 'postgres',
-        password: 'root',
-        database: 'nestjs',
-        entities: [
-            __dirname + '/../**/*.entity{.ts,.js}',
-        ],
+        type: configService.get<'postgres'>('DB_TYPE'),
+        host: configService.get('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get('DB_USER'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
         synchronize: true,
       });
 
--- src/database/database.spec.ts
+++ src/database/database.spec.ts
@@ -1,12 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { DataSource } from 'typeorm';
 import { databaseProviders } from './database.providers';
+import { ConfigModule } from '@nestjs/config';
 
 describe('Database Connection', () => {
   let dataSource: DataSource;
 
   beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [ConfigModule.forRoot()],
       providers: [...databaseProviders],
     }).compile();
 
@@ -18,8 +20,7 @@ describe('Database Connection', () => {
   });
 
   it('should be initialized', async () => {
-    const isInitialized = dataSource.isInitialized;
-    expect(isInitialized).toBe(true);
+    expect(dataSource.isInitialized).toBe(true);
   });
 
   afterAll(async () => {
